10e1d7ed493eaa17910c7180744608a05dfe1dbb
Nachiketa Vadera <personal.nachiketa@gmail.com>
2018-05-12 16:01:08 +0530

    Initial commit

---
+++ app/src/main/java/android/nachiketa/flashlight/FlashlightShakeToggle.java
@@ -0,0 +1,102 @@
+package android.nachiketa.flashlight;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
+import android.os.IBinder;
+import android.os.Vibrator;
+
+public class FlashlightShakeToggle extends Service implements SensorEventListener {
+
+    SensorManager sensorManager = null;
+    Vibrator vibrator = null;
+    public static final float SHAKE_THRESHOLD = 5.25f;
+    public static final int MIN_TIME_BETWEEN_SHAKES = 1000;
+    private long lastShakeTime = 0;
+    private boolean isFlashlightOn = false;
+
+    public FlashlightShakeToggle() {
+
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        if (sensorManager != null) {
+            Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            sensorManager.registerListener((SensorEventListener) this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            long curTime = System.currentTimeMillis();
+            if ((curTime - lastShakeTime) > MIN_TIME_BETWEEN_SHAKES) {
+
+                float x = event.values[0];
+                float y = event.values[1];
+                float z = event.values[2];
+
+                double acceleration = Math.sqrt(Math.pow(x, 2) +
+                        Math.pow(y, 2) +
+                        Math.pow(z, 2)) - SensorManager.GRAVITY_EARTH;
+
+                if (acceleration > SHAKE_THRESHOLD) {
+                    lastShakeTime = curTime;
+                    if (!isFlashlightOn) {
+                        torchToggle("on");
+                        isFlashlightOn = true;
+                    } else {
+                        torchToggle("off");
+                        isFlashlightOn = false;
+                    }
+                }
+            }
+        }
+    }
+
+    private void torchToggle(String command) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            CameraManager camManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+            String cameraId = null; // Usually back camera is at 0 position.
+            try {
+                if (camManager != null) {
+                    cameraId = camManager.getCameraIdList()[0];
+                }
+                if (camManager != null) {
+                    if (command.equals("on")) {
+                        camManager.setTorchMode(cameraId, true);   // Turn ON
+                        vibrator.vibrate(500);
+                        isFlashlightOn = true;
+                    } else {
+                        camManager.setTorchMode(cameraId, false);  // Turn OFF
+                        vibrator.vibrate(500);
+                        isFlashlightOn = false;
+                    }
+                }
+            } catch (CameraAccessException e) {
+                e.getMessage();
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Unused Method
+    }
+}
---
+++ app/src/main/java/android/nachiketa/flashlight/MainActivity.java
@@ -0,0 +1,62 @@
+package android.nachiketa.flashlight;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout relativeLayout = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        relativeLayout = (RelativeLayout) findViewById(R.id.relLayMain);
+        startService(new Intent(this, FlashlightShakeToggle.class));
+    }
+
+    public void toggle(View view) {
+        Button button = (Button) view;
+        if (button.getText().equals("Switch On")) {
+            relativeLayout.setBackgroundColor(Color.WHITE);
+            button.setText("Switch Off");
+            torchToggle("on");
+        } else {
+            button.setText("Switch On");
+            relativeLayout.setBackgroundColor(Color.BLACK);
+            torchToggle("off");
+        }
+    }
+
+    private void torchToggle(String command) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            CameraManager camManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+            String cameraId = null; // Usually back camera is at 0 position.
+            try {
+                if (camManager != null) {
+                    cameraId = camManager.getCameraIdList()[0];
+                }
+                if (camManager != null) {
+                    if (command.equals("on")) {
+                        camManager.setTorchMode(cameraId, true);   // Turn ON
+                    } else {
+                        camManager.setTorchMode(cameraId, false);  // Turn OFF
+                    }
+                }
+            } catch (CameraAccessException e) {
+                e.getMessage();
+            }
+        }
+    }
+}
